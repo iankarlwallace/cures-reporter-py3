4b5311f5047609ebaa82229372d474e2de45eaaa
Ian Wallace <iankarlwallace@gmail.com>
2019-05-13 16:18:47 -0700

    Abstracting out the browser component so that can be reused across Cures and EMR logins

--- emrscrapper/common/browser.py
+++ emrscrapper/common/browser.py
@@ -2,7 +2,36 @@
 # Setup the profile and maximize it
 # Should include basics of downloading ICA file and XLSX files automatically
 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 class Browser:
 
-    def __init__(self)
-        None
+    def __init__(self):
+        self.prof = self._setup_profile()
+        self.wd = webdriver.Firefox(firefox_profile = self.prof)
+        self.wd.maximize_window()
+        self.wd.implicitly_wait(3)
+
+    def __del__(self):
+        self.wd.quit()
+
+    def _setup_profile(self):
+        prof = webdriver.FirefoxProfile()
+        prof.set_preference('browser.helperApps.neverAsk.openFile','application/octet-stream,application/x-ica')
+        return prof
+
+    def goto_url(self, url):
+        self.wd.get(url)
+
+    def fill_element_by_name(self, elemName, value):
+        element = self.wd.find_element_by_name(elemName)
+        element.clear()
+        element.send_keys(value)
+
+    def click_element_by_xpath(self, elemXpath, value):
+        element = self.wd.find_element_by_xpath(elemXpath)
+        element.click()
+
+    def quit(self):
+        self.__del__
--- emrscrapper/common/curestools.py
+++ emrscrapper/common/curestools.py
@@ -1,6 +1,7 @@
 # CURES Tools - tools for gathering prescription data from Cures website based on
 # Patient name, DOB returning a list of prescriptions filled in a certain amount of time
 
+import common.browser
 import config
 import time
 from selenium import webdriver
@@ -9,32 +10,17 @@ from selenium.webdriver.common.keys import Keys
 class Curestools:
 
     def __init__(self, creds):
-        self.fp = self._setup_firefox_profile()
-        self.wd = webdriver.Firefox(firefox_profile = self.fp)
-        self.wd.maximize_window()
-        self.wd.implicitly_wait(3)
+        self.browser = common.browser.Browser()
         self.creds = creds
 
     def __del__(self):
-        self.wd.quit()
-
-    def _setup_firefox_profile(self):
-        fp = webdriver.FirefoxProfile()
-        fp.set_preference('browser.helperApps.neverAsk.openFile','application/octet-stream,application/x-ica')
-        return fp
-
-    def _fill_element_by_name(self, elemName, value):
-       element = self.wd.find_element_by_name(elemName)
-       element.clear()
-       element.send_keys(value)
+        self.browser.quit()
 
     def login(self):
-        self.wd.get(config.CURES_HOME_URL)
-        assert 'State of California Department of Justice TST02' in self.wd.title
-        self._fill_element_by_name('USER', self.creds.getCuresUsername())
-        self._fill_element_by_name('PASSWORD', self.creds.getCuresPassword())
-        elemLogin = self.wd.find_element_by_xpath('//*[@id="loginForm"]/div[1]/span/input')
-        elemLogin.click()
+        self.browser.goto_url(config.CURES_HOME_URL)
+        assert 'State of California Department of Justice TST02' in self.browser.wd.title
+        self.browser.fill_element_by_name('USER', self.creds.getCuresUsername())
+        self.browser.fill_element_by_name('PASSWORD', self.creds.getCuresPassword())
+        self.broser.click_element_by_xpath('//*[@id="loginForm"]/div[1]/span/input')
         time.sleep(10)
         return
-
